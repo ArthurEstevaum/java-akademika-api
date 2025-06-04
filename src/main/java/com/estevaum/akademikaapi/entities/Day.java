17115e224003baaa3d3c29e6576d018ceaba443f
Estevaum <arthurestevao631@gmail.com>
2025-06-04 16:56:18 -0300

    Fix dev db issue

--- src/main/java/com/estevaum/akademikaapi/entities/Day.java
+++ src/main/java/com/estevaum/akademikaapi/entities/Day.java
@@ -18,7 +18,7 @@ public class Day {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "day_of_week")
     private Days day;
 
     @ManyToMany(mappedBy = "days")
